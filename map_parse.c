de3ec8778f59ce5f7dda0aa7f3d611c30c7927a3
Eduardo Enrique Penaloza Brito <epenaloz@c1r6s3.42madrid.com>
2024-07-10 16:44:50 +0200

    preparado para rendering

--- map_parse.c
+++ map_parse.c
@@ -231,22 +231,3 @@ void valid_check(t_map *map)
 	check_chars(map, "");
 	print_map(map->copy);
 }
-
-int main(int ac, char **av)
-{
-    t_map   map;
-
-    init_map(&map);
-    if (ac != 2)
-    {
-        error("invalid format. Format: ./so_long [MAP]", &map);
-        return (1);
-    }
-        ft_printf("for leaks, use the command: valgrind --leak-check=full --show-leak-kinds=all ./so_long %s\n", av[1]);
-        readmap(av[1], &map);
-        check_borders_and_tiles(&map);
-        print_map(map.map);
-        valid_check(&map);
-        clean_all(&map);
-    return (0);
-}
